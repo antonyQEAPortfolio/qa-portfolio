3f50878d393e669ade17e0a9780fa918c00aa0e0
antonyQEAPortfolio <antony.sagayaraj7@gmail.com>
2025-05-05 22:51:51 -0400

    Fixed the error in addinitScript method

--- playwright-framework/tests/E2ECart.spec.js
+++ playwright-framework/tests/E2ECart.spec.js
@@ -28,9 +28,9 @@ test('@Web Client App login', async ({ page }) => {
    const bool = await page.locator("h3:has-text('ZARA COAT 3')").isVisible();
    expect(bool).toBeTruthy();
    await page.locator("text=Checkout").click(); 
-   await page.locator("[placeholder*='Country']").fill("ind"); 
+   await page.locator("[placeholder*='Country']").pressSequentially("ind", { delay: 100 });
    const dropdown = page.locator(".ta-results");
-   await dropdown.waitFor();
+   await dropdown.waitFor({ state: "visible", timeout: 5000 });
    const optionsCount = await dropdown.locator("button").count();
    for (let i = 0; i < optionsCount; ++i) {
       const text = await dropdown.locator("button").nth(i).textContent();
--- playwright-framework/tests/createOrderThroughApi.spec.js
+++ playwright-framework/tests/createOrderThroughApi.spec.js
@@ -1,9 +1,8 @@
 const {test, expect, request} = require('@playwright/test');
 const { ApiUtils } = require('../utils/ApiUtils.js');
-const loginPayLoad = {userEmail:"anshika@gmail.com",userPassword:"Iamking@000"};
+const loginPayLoad = {userEmail:"antony.sagayaraj7@gmail.com",userPassword:"India@1947."};
 const orderPayLoad = {orders:[{country:"Cuba",productOrderedId:"67a8dde5c0d3e6622a297cc8"}]};
- 
- 
+
 let response;
 test.beforeAll( async()=>
 {
@@ -11,22 +10,19 @@ test.beforeAll( async()=>
    const apiUtils = new ApiUtils(apiContext,loginPayLoad);
    response =  await apiUtils.createOrder(orderPayLoad);
  
-})
- 
- 
+}) 
 //create order is success
 test('@API Place the order', async ({page})=>
 { 
+    await page.goto("https://rahulshettyacademy.com/client");
     page.addInitScript(value => {
- 
-        window.localStorage.setItem('token',value);
+         window.localStorage.setItem('token',value);
     }, response.token );
-await page.goto("https://rahulshettyacademy.com/client");
+    await page.reload();
+  await page.goto("https://rahulshettyacademy.com/client");
  await page.locator("button[routerlink*='myorders']").click();
  await page.locator("tbody").waitFor();
-const rows = await page.locator("tbody tr");
- 
- 
+const rows = await page.locator("tbody tr"); 
 for(let i =0; i<await rows.count(); ++i)
 {
    const rowOrderId =await rows.nth(i).locator("th").textContent();
