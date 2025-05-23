5e39136b4676a093d6b420a7a49e59016607b27f
antonyQEAPortfolio <antony.sagayaraj7@gmail.com>
2025-05-23 14:58:49 -0400

    Updated script
    Ai basic script update

--- playwright-framework/tests/aiPlaywrightBasic.spec.js
+++ playwright-framework/tests/aiPlaywrightBasic.spec.js
@@ -2,15 +2,16 @@ const { test, expect } = require('@playwright/test');
 import {ai} from "@zerostep/playwright"
 require('dotenv').config();
 
-test('zerostep example', async ({ page }) => {
+test('Login Using AI', async ({ page }) => {
   const userLogin = process.env.Login_email;
   const userPassword = process.env.Login_password;
-  await page.goto('https://rahulshettyacademy.com/loginpagePractise/')
   const aiArgs = { page, test }
-  await ai(`Enter "${userLogin}" as Username`, aiArgs)
-  await ai(`Enter "${userPassword}" as Password`, aiArgs)
-  await ai('Click Sign In', aiArgs)
-  await page.waitForTimeout(8_000)
+  await page.goto('https://rahulshettyacademy.com/loginpagePractise/')
+  await ai(`Enter "${userLogin}" as Username`, aiArgs);
+  await ai(`Enter ${userPassword} into the field with tag id as 'password'`, aiArgs);
+  await page.waitForTimeout(3000);
+  await ai(`Click the button with id "signInBtn"`, aiArgs);
+  await page.waitForTimeout(3000)
   await page.evaluate(() => window.scrollTo(0, document.body.scrollHeight));
   await ai('click Add button associated with "iphone X"',aiArgs)
   await ai('Go to the checkout page',aiArgs)
