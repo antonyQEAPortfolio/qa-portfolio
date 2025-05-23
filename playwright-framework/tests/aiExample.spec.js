f98ecb4f475acb27795f77736fd7e85ca68e10a0
antonyQEAPortfolio <antony.sagayaraj7@gmail.com>
2025-05-22 23:27:59 -0400

    Added AI TestCase
    Added AI Test Case using zerostep

---
+++ playwright-framework/tests/aiExample.spec.js
@@ -0,0 +1,18 @@
+const { test, expect } = require('@playwright/test');
+import {ai} from "@zerostep/playwright"
+
+test('AI Test capability', async ({ page }) => {
+  const aiArgs = {page,test}
+  await page.goto('https://rahulshettyacademy.com/seleniumPractise/#/offers');
+  const blinkingText = await ai("Get blinkingText in the page",aiArgs)
+  expect(blinkingText).toEqual("Career Focussed QA Meetup with Rahul Shetty @Pune - Limited Seats! Book Now!")
+  const discountPrice = await ai("What is the Discount price of Tomato",aiArgs)
+  expect(discountPrice).toEqual("26")
+  const price = await ai("What is the Price of Tomato",aiArgs)
+  expect(price).toEqual("37")
+  const diff = await ai("What is the value difference between price and Discounr price of tomato",aiArgs)
+  expect(diff).toEqual("11")
+  
+
+});
+
