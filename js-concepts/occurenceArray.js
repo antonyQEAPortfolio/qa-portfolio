57dd781ff46f7dd2d3cbcef2b36fce0b9b310cfa
antonyQEAPortfolio <antony.sagayaraj7@gmail.com>
2025-10-25 18:38:28 -0400

    updated js file

--- js-concepts/occurenceArray.js
+++ js-concepts/occurenceArray.js
@@ -2,11 +2,11 @@ const numbers = [1, 2, 3, 4, 5, 1, 2, 3, 4, 5];
 
 // Syntax of reduce function
 
-array.reduce((accumlator , currentvalue) => {
-    return accumlator;
-}, initialValue);
+// array.reduce((accumlator , currentvalue) => {
+//     return accumlator;
+// }, initialValue);
 
-numbers.reduce((accumlator , currentvalue) => {
+const count = numbers.reduce((accumlator , currentvalue) => {
     accumlator[currentvalue] = (accumlator[currentvalue] || 0) + 1;
     return accumlator;
 }, {});
